620727507cff7b234ae87d96f2798006639e3503
T. Dũng <85283786+phantatdung233@users.noreply.github.com>
2025-04-17 20:04:54 +0700

    Update: Fix bugs

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -4,10 +4,12 @@
 // @version      3.0
 // @description  Bypass Yeumoney
 // @author       xGreen
-// @match        https://yeumoney.com/*
 // @require      https://raw.githubusercontent.com/phantatdung233/bypass-yeumoney/refs/heads/main/bypass-yeumoney-v3.js
+// @match        https://yeumoney.com/*
 // @grant        GM_xmlhttpRequest
-// @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @run-at       document-end
 // @icon         https://avatars.githubusercontent.com/u/85283786?s=48&v=4
 // ==/UserScript==
