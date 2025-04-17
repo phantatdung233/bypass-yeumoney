bd8378a9cc1f18a64f18dc0d0d6d67b6482cdd65
T. Dũng <85283786+phantatdung233@users.noreply.github.com>
2025-04-17 19:47:21 +0700

    Create: Installation file

---
+++ tampermonkey.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Bypass Yeumoney V3
+// @namespace    http://tampermonkey.net/
+// @version      3.0
+// @description  Bypass Yeumoney
+// @author       xGreen
+// @match        https://yeumoney.com/*
+// @require      https://raw.githubusercontent.com/phantatdung233/bypass-yeumoney/refs/heads/main/bypass-yeumoney-v3.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @icon         https://avatars.githubusercontent.com/u/85283786?s=48&v=4
+// ==/UserScript==
